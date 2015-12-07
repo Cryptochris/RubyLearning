d82d5aa92bf6f2965cf7dae1a2482da92834f5fb
ChrisG <cryptochrisg@gmail.com>
2015-12-07 15:39:51 -0800

    Now with file check and global insert.
    We made the method more usable without having it be so narrow.
    The program finds a file and a certain word and allows you to
    change all instances that word.

--- 3_week/2e_inserted_word.rb
+++ 3_week/2e_inserted_word.rb
@@ -21,12 +21,30 @@ require './2_week/1c_prompt'
 def insert_word(file, target_word, inserted_word)
   file_to_string = File.read(file)
   File.open(file, 'w') do |f|
-    f.puts( file_to_string.sub(target_word, inserted_word + ' ' + target_word))
+    f.puts( file_to_string.gsub(target_word, inserted_word + ' ' + target_word))
   end
 end
 
+# make this input as well, with maybe text.txt as a 'default' value for unattended running
+file = prompt("What file are we modifying?")
+if file.empty?
+  file = 'text.txt'
+end
+unless File.exist?(file)
+  puts "There is no file called #{file}.  Exiting program."
+  exit(0) # -> We want to get out of this program.
+end
+puts '=+' * 40
+puts File.open(file, 'r').each_line.to_a
+puts '=+' * 40
+
+puts "Your file is above"
 target_word = prompt "What is the target we are looking for?"
 inserted_word = prompt "What is the word you would like to insert in front of '#{target_word}'"
-# make this input as well, with maybe text.txt as a 'default' value for unattended running
-file = 'text.txt'
+
 insert_word(file, target_word, inserted_word)
+
+puts "Here is the resulting file!"
+puts '=+' * 40
+puts File.open(file, 'r').each_line.to_a
+puts '=+' * 40
